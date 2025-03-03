5e4718c7596de6c6fc3a613e9feae408a1191b02
marangstavo <marangwandasteve@gmail.com>
2025-03-03 23:20:11 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -52,6 +52,19 @@
             $dropdown.off("mouseenter mouseleave");
         }
     });
+
+    function slideSlider(){
+  $("#slider-scroller").css({"left":"0%","transition":"all 0s linear"});
+  $("#slider-scroller").css({"left": String(parseInt($("#slider-scroller").css("left")) - 500) + "px","transition":"all 5s linear"});
+  setTimeout(function(){moveSliderItem()}, 2635);
+}
+
+function moveSliderItem(){
+  $("#slider-scroller div").first().detach().appendTo($("#slider-scroller"));
+  slideSlider();
+}
+
+slideSlider();
     
     
     // Back to top button
